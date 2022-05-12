157c034a764cc18f65629e89758684cd44ee4e59
Santos <erick_santos00@outlook.com>
2022-05-11 22:24:38 -0300

    feat(common): Commit init

---
+++ functions/logger.js
@@ -0,0 +1,33 @@
+import { CLI_COLOURS, LOG_LEVEL } from '../constants/main.js';
+import { getCurrentDate } from './date.js';
+import { getCurrentHour } from './time.js';
+
+function getColorPerLevel(logLevel) {
+    switch (logLevel) {
+        case LOG_LEVEL.LOG_ERR:
+            return CLI_COLOURS.fg.red;
+        case LOG_LEVEL.LOG_INFO:
+            return CLI_COLOURS.fg.blue;
+        case LOG_LEVEL.LOG_WARN:
+            return CLI_COLOURS.fg.yellow
+        default:
+            return CLI_COLOURS.fg.white
+    }
+}
+
+export default function logger(logLevel, msg) {
+    if (!Object.values(LOG_LEVEL).includes(logLevel)) {
+        console.log(`${logLevel} não incluso nos niveis definidos.`);
+        logLevel = LOG_LEVEL.LOG_INFO;
+    }
+    const date = getCurrentDate();
+    const time = getCurrentHour();
+    if (typeof msg == "object") {
+        msg = JSON.stringify(msg);
+    }
+    console.log(
+        getColorPerLevel(logLevel), 
+        `[${date}] - [${time}]: ${logLevel} - ${msg}`,
+        CLI_COLOURS.reset
+    );
+}
